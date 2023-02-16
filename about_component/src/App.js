9ea25e3df5df6c659d6a269bdd1795628c88cc5b
codescalper <msfunbook@gmail.com>
2023-02-16 12:52:13 +0530

    Learned about Props

--- about_component/src/App.js
+++ about_component/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Heading from "./Heading"
+
 function Hello() {
   return <h1>Hello World</h1>;
  }
-
 function App() {
- return <Hello />;
+ return <Heading firstName="Mayank" Dish="PavBhaji" / >;
 }
 
 export default App;
---
+++ about_component/src/Heading.js
@@ -0,0 +1,11 @@
+function Heading(props){
+    return (
+        <div>
+        <h1>Hello, {props.firstName} </h1>
+        <h2>My favourite dish is {props.Dish}</h2>
+        </div>
+    )
+}
+
+
+export default Heading;
