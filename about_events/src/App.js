e4bffc5f5b95004e1e38ec3f78a4f6b5206ef827
codescalper <msfunbook@gmail.com>
2023-02-17 18:54:30 +0530

    Random number generator

--- about_events/src/App.js
+++ about_events/src/App.js
@@ -3,15 +3,32 @@ import './App.css';
 import Button from "./Button"
 import ModeToggler from './ModeToggler';
 
-function App() {
-  return (
-    <div>
-    <Button />
-    <ModeToggler />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//     <Button />
+//     <ModeToggler />
+//     </div>
+//   );
+// }
 
-  
+
+function handleClick() { 
+  let randomNum = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNum);
+  let userInput = prompt('type a number'); 
+  alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+}
+
+
+function App() {
+
+return (
+  <div>
+    <h1>Task: Add a button and handle a click event</h1>
+    <button onClick={handleClick}>Guess the number between 1 and 3.</button>
+  </div>
+);
 }
 
 export default App;
