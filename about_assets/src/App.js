42c3962830fc5c8b408771fbdc3d37866a1127fc
codescalper <msfunbook@gmail.com>
2023-02-19 19:06:57 +0530

    Embedding video using 3P packages

--- about_assets/src/App.js
+++ about_assets/src/App.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import mayank from './assets/images/pfp.jpg';
 
+
+
+const MyVideo = () => {
+  return (
+    <ReactPlayer url='https://youtu.be/W8lMMzYtatw' />
+  );
+};
+
 function App() {
   const onlineImg = "https://cdn.discordapp.com/attachments/1037708259152367677/1074984214912565258/Untitled-01.png";
   return (
@@ -11,10 +19,12 @@ function App() {
       <img src={mayank} height={200} width={200} alt="My profile picture" />
 
       {/* Another way of adding image using require attribute */}
-      <img height={200} width={200} alt="My profile picture" require={"./assets/images/pfp.jpg"}/>
+      <img height={200} width={200} alt="My profile picture" require={"/assets/images/pfp.jpg"}/>
 
       {/* ANother way of adding image and the image can be from a website */}
-      <img height={200} width={200} alt="Photo from discord" require={onlineImg}/>
+      <img height={200} width={200} alt="Photo from discord" require={onlineImg} />
+
+      <MyVideo />
 
     </div>
   );
